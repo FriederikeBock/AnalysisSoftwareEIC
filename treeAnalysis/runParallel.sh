d692a530618224f8b3bdbf461547bb85bcca2c83
Tristan Protzman <tlprotzman@gmail.com>
2021-07-26 14:29:07 -0400

    add flags for cores and file list

--- treeAnalysis/runParallel.sh
+++ treeAnalysis/runParallel.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
-NUMTHREADS=16
-INPUT="/home/tristan/ecce/Singularity/analysis/AnalysisSoftwareEIC/treeAnalysis/test_prod.txt"
+# NUMTHREADS=16
+# INPUT="/home/tristan/ecce/Singularity/analysis/AnalysisSoftwareEIC/treeAnalysis/test_prod.txt"
 
 echo "IMPORTANT NOTE: Only tested for jet resolution studies, other studies may not combined histograms in a sensible way."
 echo "If verified for other studies, please leave a note here."
@@ -15,6 +15,13 @@ function stop {
 }
 trap stop SIGHUP SIGINT SIGTERM
 
+while getopts j:f: option; do
+    case "${option}" in
+        j) NUMTHREADS=${OPTARG};;
+        f) INPUT=${OPTARG};;
+    esac
+done
+
 # Staging areas
 mkdir -p treeProcessing/input_files
 mkdir -p treeProcessing/logs
@@ -27,7 +34,7 @@ cd ../..
 PIDS=()
 
 # Start NUMTHREADS instances of the analyzer 
-for (( i = 0; i < $NUMTHREADS; i++ )); do
+for (( i=0; i<$NUMTHREADS; i++ )); do
   printf -v J "%02d" $i
   root -b -x -q -l 'treeProcessing.C("treeProcessing/input_files/x'$J'", "geometry.root", "'$J'", true, true)' > "treeProcessing/logs/$J.log" 2> "treeProcessing/logs/$J.err" &
   PIDS+=( $! )
@@ -39,11 +46,11 @@ echo "Done processing, merging histograms..."
 # Merge histograms with hadd
 for FILE in  output_CS.root output_HITS.root output_JRH.root output_TMSTUD.root output_TRKRS.root output_CLSIZER.root output_EventObservables.root output_JetObservables.root output_RH.root output_TRKEFF.root output_TRKS_Comparison.root; do
     FILEPATHS=()
-    for (( i = 0; i < $NUMTHREADS; i++)); do 
+    for (( i=0; i<$NUMTHREADS; i++)); do 
         printf -v FILEPATH "treeProcessing/%02d/%s" $i $FILE
         FILEPATHS+=( $FILEPATH )
     done
-    # echo ${FILEPATHS[@]}
+    echo ${FILEPATHS[@]}
     hadd -j $NUMTHREADS -f treeProcessing/$FILE ${FILEPATHS[@]}
 done
 
